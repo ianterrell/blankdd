9a4df2beaef9cf4dcf7a4c01a97934f41f427152
Ian Terrell <ian.terrell@gmail.com>
2011-04-14 15:21:45 -0400

    update blank taglines

--- app/controllers/disciplines_controller.rb
+++ app/controllers/disciplines_controller.rb
@@ -10,15 +10,29 @@ class DisciplinesController < ApplicationController
     @discipline = Discipline.new(params[:discipline])
     flash[:alert] = if @discipline.expanded[0..0].downcase != @discipline.initial.downcase
       "Don't you know what an initialism is?"
-    elsif Discipline.find_by_downcased(@discipline.expanded.downcase)
-      "Sheeple! Someone else already invented that."
+    elsif @existing = Discipline.find_by_downcased(@discipline.expanded.downcase)
+      if @existing.tagline.blank? && !@discipline.tagline.blank?
+        @existing.tagline = @discipline.tagline
+        if @existing.tweet.size > 140
+          "tl;dt! Too long, doesn't tweet!"
+        else
+          if @existing.save
+            flash[:notice] = "You stand on the shoulders of giants. Now <a href=\"https://twitter.com?status=#{@existing.tweet}\">profess your belief.</a>".html_safe
+            nil
+          else
+            "FAIL!"
+          end
+        end
+      else
+        "Sheeple! Someone else already invented that."
+      end
     elsif @discipline.tweet.size > 140
       "tl;dt! Too long, doesn't tweet!"
     elsif !@discipline.save
       "FAIL!"
     end
     unless flash[:alert]
-      flash[:notice] = "So you say. Now <a href=\"https://twitter.com?status=#{@discipline.tweet}\">profess your belief.</a>".html_safe
+      flash[:notice] ||= "So you say. Now <a href=\"https://twitter.com?status=#{@discipline.tweet}\">profess your belief.</a>".html_safe
     end
     redirect_to root_path
   end
